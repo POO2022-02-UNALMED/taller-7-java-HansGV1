0b71c3e37d1cf206fdd365249c13a305c2c5353f
Hans García <82074617+HansGV1@users.noreply.github.com>
2022-11-14 23:13:42 -0500

    Up

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,49 @@
+package comunicacion;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Alfabeto extends Pictograma{
+	private static List<String> letras;
+	private String interpretacion;
+	public final char[] letrasAbecedario = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+	public String letters;
+	
+	public Alfabeto(String ori, List<String> letr, String inter) {
+		super(ori);
+		Alfabeto.letras = letr;
+		this.interpretacion = inter;
+	}
+	
+	public static List<String> getLetras() {
+		return Alfabeto.letras;
+	}
+	
+	public static void setLetras(List<String> le) {
+		Alfabeto.letras = le;
+	}
+	
+	public String getInterpretacion() {
+		return this.interpretacion;
+	}
+	public void setInterpretacion(String inte) {
+		this.interpretacion = inte;
+	}
+	
+	public int cantidadLetras() {
+		return Alfabeto.letras.size();
+	}
+	
+	@Override
+	public String interpretacion() {
+		return this.interpretacion;
+	}
+	@Override
+	public String toString() {
+		for (int i = 0; i < 26; i++ ) {
+			this.letters = this.letters + letrasAbecedario[i] + ", ";
+		}
+		return this.letrasAbecedario.toString();
+	}
+	
+	
+}
---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,40 @@
+package comunicacion;
+
+public abstract class Fabula extends Escrito{
+	private String ensenanza;
+	private String interpretacion;
+	
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion){
+		super(origen, titulo, autor, paginas);
+		this.ensenanza = ensenanza;
+		this.interpretacion = interpretacion;
+	}
+	
+	public String getEnsenanza() {
+		return this.ensenanza;
+	}
+	public void setEnsenanza(String en) {
+		this.ensenanza = en;
+	}
+	public String getInterpretacion() {
+		return this.interpretacion;
+	}
+	public void setInterpretacion(String inte) {
+		this.interpretacion = inte;
+	}
+	
+	@Override
+	public int palabrasTotales(int pal) {
+		return super.getPaginas()*1;
+	}
+	
+	@Override
+	public String interpretacion() {
+		return this.interpretacion;
+	}
+	
+	@Override
+	public String toString() {
+		return super.getOrigen() + "\n" + super.getTitulo() + "\n"  + super.getAutor() + "\n"  + super.getPaginas() + "\n"  + this.ensenanza ;
+	}
+}
