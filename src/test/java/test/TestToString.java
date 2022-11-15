0ff692dfafc65cc616de29cb525268296a21489e
Hans García <82074617+HansGV1@users.noreply.github.com>
2022-11-15 00:10:37 -0500

    Up

--- src/test/java/test/TestToString.java
+++ src/test/java/test/TestToString.java
@@ -46,7 +46,6 @@ public class TestToString {
 				"2\n" + 
 				"no se debe uno burlar de los demas, ni presumir o ser vanidoso";
 		boolean ok = false;
-		System.out.println(fabula.toString());
 		if (fabula.toString().equals(comp)) {
 			ok = true;
 		}
